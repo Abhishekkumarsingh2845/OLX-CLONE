36ffabe64561c1c384400af9e69332595b132bf1
Abhishekkumarsingh2845 <109286249+Abhishekkumarsingh2845@users.noreply.github.com>
2024-05-12 11:46:51 +0530

    image picker has added

--- Src/Tabs/Add.js
+++ Src/Tabs/Add.js
@@ -1,21 +1,98 @@
-import { View, Text, StyleSheet, Image } from 'react-native'
-import React from 'react'
-import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import {View, Text, StyleSheet, Image, PermissionsAndroid} from 'react-native';
+import React, {useState} from 'react';
+import {TextInput, TouchableOpacity} from 'react-native-gesture-handler';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import {type} from 'os';
 
 const Add = () => {
+  const [photo, setPhoto] = useState({
+    assets: [
+      {
+        fileName: '',
+        fileSize: 2242730,
+        height: 4000,
+        originalPath: '',
+        type: 'image/jpeg',
+        uri: '',
+        width: 2250,
+      },
+    ],
+  });
+  const[name,setName]=useState('');
+  const[price,setPrice]=useState('');
+  const[desc,setdesc]=useState('');
+
+  const requestCameraPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        {
+          title: 'Cool Photo App Camera Permission',
+          message:
+            'Cool Photo App needs access to your camera ' +
+            'so you can take awesome pictures.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can use the camera');
+        opencamera();
+      } else {
+        console.log('Camera permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  const opencamera = async () => {
+    const result = await launchCamera({mediaType: 'photo'});
+    if(!result.didCancel)
+      {
+        setPhoto(result)
+      }
+   
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.text}>Add</Text>
       </View>
-      <TouchableOpacity style={styles.touchable}>
-        <Image source={require('../images/ll.jpg')} style={styles.image} />
+      <TouchableOpacity
+        style={styles.touchable}
+        onPress={requestCameraPermission}>
+        {photo.assets[0].uri == '' ? (
+          <Image source={require('../images/ll.jpg')} style={styles.image} />
+        ) : (
+          <Image source={{uri: photo.assets[0].uri}} style={styles.image} />
+        )}
       </TouchableOpacity>
-      <TextInput placeholder="Enter Item Name" placeholderTextColor='black' style={styles.xx}/>
-      <TextInput placeholder="Enter Item Desc" placeholderTextColor='black' style={styles.xx}/>
-      <TextInput placeholder="Enter Item Price" placeholderTextColor='black' style={styles.xx}
-      keyboardType='number-pad'/>
-       <TouchableOpacity style={styles.blueButton}>
+      <TextInput
+        placeholder="Enter Item Name"
+        placeholderTextColor="black"
+        style={styles.xx}
+        value={name}
+        onChangeText={txt=>setName(txt)}
+      />
+      <TextInput
+        placeholder="Enter Item Desc"
+        placeholderTextColor="black"
+        style={styles.xx}
+        value={desc}
+        onChangeText={txt=>setdesc(txt)}
+      />
+      <TextInput
+        placeholder="Enter Item Price"
+        placeholderTextColor="black"
+        style={styles.xx}
+        keyboardType="number-pad"
+        value={price}
+        onChangeText={txt=>setPrice(txt)}
+      />
+      <TouchableOpacity style={styles.blueButton}>
         <Text style={styles.buttonText}>Post Item</Text>
       </TouchableOpacity>
     </View>
@@ -25,7 +102,7 @@ const Add = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent:'flex-start',
+    justifyContent: 'flex-start',
     alignItems: 'start',
   },
   header: {
@@ -45,7 +122,7 @@ const styles = StyleSheet.create({
     height: 150, // Increased height for demonstration
     alignSelf: 'center',
     marginTop: 20,
-  
+
     justifyContent: 'center', // Added justifyContent for centering the image
     alignItems: 'center', // Added alignItems for centering the image
   },
@@ -54,14 +131,14 @@ const styles = StyleSheet.create({
     height: '100%', // Set height to 100% to fit the TouchableOpacity
     resizeMode: 'contain',
   },
-  xx:
-  {
-    width:300,
-    height:50,
-    borderWidth:1,
-    alignSelf:'center',
-    borderRadius:5,
-    marginTop:20,paddingLeft:20,
+  xx: {
+    width: 300,
+    height: 50,
+    borderWidth: 1,
+    alignSelf: 'center',
+    borderRadius: 5,
+    marginTop: 20,
+    paddingLeft: 20,
   },
   blueButton: {
     backgroundColor: 'blue',
@@ -76,7 +153,7 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 16,
     color: 'white',
-  }
+  },
 });
 
 export default Add;
